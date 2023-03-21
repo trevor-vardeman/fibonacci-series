e9edbb33f1bbe82beed05d43df137ea2a3833898
Trevor Vardeman <trevortx@gmail.com>
2023-03-20 18:36:55 -0700

    complete

--- ruby/fibonacci.rb
+++ ruby/fibonacci.rb
@@ -1,5 +1,21 @@
 def fibonacci(num)
-  # type your code in here
+  first_num = 0
+  second_num = 0
+  current_sum = 0
+  count = 0
+  until count == num
+    if count == 0
+      second_num += 1
+      current_sum = first_num + second_num
+      count += 1
+    else
+      current_sum = first_num + second_num
+      first_num = second_num
+      second_num = current_sum
+      count += 1
+    end
+  end
+  current_sum
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -15,9 +31,4 @@ if __FILE__ == $PROGRAM_NAME
 
   puts "Expecting: 55"
   puts "=>", fibonacci(10)
-
-  # Don't forget to add your own!
 end
-
-# Please add your pseudocode to this file
-# And a written explanation of your solution
